442a0f9bf45af1583c6acd0de4e2b0b667a60275
Puteri Husnul Khotimah <husnulputeri@gmail.com>
2022-07-18 18:45:39 +0700

    Node JS Basics
    Run File Node

---
+++ coba.js
@@ -0,0 +1,6 @@
+// // Cara Menjalankan satu file Javascript lewat Node JS
+// // Menggunakan Terminal
+// console.log('Hello World!');
+
+// const nama = 'Puteri Husnul';
+// console.log(nama);
---
+++ index.js
@@ -0,0 +1,8 @@
+// // Cara Menjalankan satu file Javascript lewat Node JS
+// // Menggunakan Terminal
+// console.log('Hello WPU!');
+
+// memanggil dari ambil parameter
+const cetakNama = (nama) => `Hi, Nama saya ${nama}`;
+// Tidak memakai {} dan return karena hanya sebaris ekspresinya
+console.log(cetakNama("Puteri Husnul"));
